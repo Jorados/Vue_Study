fe25deee6c3dfb2ebfeb9eb8b0d421f15a445d7e
seongjin8860 <seongjin8860@naver.com>
2024-07-17 22:16:58 +0900

    first commit

---
+++ section_2/component/SiteHeader.js
@@ -0,0 +1,15 @@
+import Nav from "./header/Nav.js";
+
+export default {
+  template: `
+  <div>header</div>
+  <Nav/>
+  <Nav/>
+  <Nav/>
+  <Nav/>
+  <Nav/>
+  `,
+  components: {
+    Nav: Nav,
+  },
+};
